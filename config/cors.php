d37da4af2e0723e3af00651ff68b4404bdce8149
CodeDoux <sarrcodou98@gmail.com>
2026-06-10 17:29:26 +0100

    fix: change variables allowed_origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://toulayskin.sn',
+    'https://www.toulayskin.sn',
+    ],
 
     'allowed_origins_patterns' => [],
 
